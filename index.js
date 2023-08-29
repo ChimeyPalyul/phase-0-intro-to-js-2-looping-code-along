5f23cb7b416cf65ebda873400e399ae91b87dda0
Chimey Palyul <palyul.chimey@gmail.com>
2023-08-29 11:21:00 -0600

    finally done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const name = ["Guadalupe","Ollie","Aki"];
+const event =["surprise"]
+const finalArray = []
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length ; i++) {
+        finalArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);   
+    }
+    return finalArray;
+}
+
+
+let integer = 10
+function countDown(integer){
+    while(integer > -1){
+    console.log(integer --);
+    }
+}
